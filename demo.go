a328afe5bb9a4a65be295ee8011617d8d4f7507b
axetroy <troy450409405@gmail.com>
2019-08-15 12:01:44 +0800

    init

---
+++ demo.go
@@ -0,0 +1,32 @@
+// test.go
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var (
+	listen = flag.String("listen", ":8080", "listen address")
+	dir    = flag.String("dir", "./demo", "directory to serve")
+)
+
+func main() {
+	flag.Parse()
+	log.Printf("listening on %q...", *listen)
+
+	err := http.ListenAndServe(*listen, http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		if strings.HasSuffix(req.URL.Path, ".wasm") {
+			resp.Header().Set("content-type", "application/wasm")
+		}
+
+		http.FileServer(http.Dir(*dir)).ServeHTTP(resp, req)
+	}))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
